94adb09ae21bce8434466f80b12fe8ece93a5ffc
Jaska <jason@claralabs.com>
2016-07-27 18:25:30 -0700

    Add basic tests

---
+++ spherecluster/tests/test_common.py
@@ -0,0 +1,11 @@
+from sklearn.utils.estimator_checks import check_estimator
+from .spherical_kmeans import SphericalKMeans
+from .von_mises_fisher_mixture import VonMisesFisherMixture
+
+
+def test_estimator_spherical_k_means():
+    return check_estimator(SphericalKMeans)
+
+
+def test_estimator_vmf():
+    return check_estimator(VonMisesFisherMixture)
