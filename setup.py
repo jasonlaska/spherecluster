2c759bf71d6f33136f44e9b7552526f580d1f8a6
Jaska <jason@claralabs.com>
2016-08-08 20:44:31 -0700

    Add setuptools files

---
+++ setup.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import sys
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
+
+
+try:
+    import numpy
+except ImportError:
+    print('numpy is required during installation')
+    sys.exit(1)
+
+try:
+    import scipy
+except ImportError:
+    print('scipy is required during installation')
+    sys.exit(1)
+
+setup(name='spherecluster',
+      version='0.1.0',
+      description='Clustering on the unit hypersphere in scikit-learn.',
+      author='Jason Laska',
+      packages=find_packages(),
+      install_requires=INSTALL_REQUIRES,
+      author_email='jason@claralabs.com',
+      )
