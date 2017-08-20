bf55f9b500879e2c8e5e5c23e4c48d8d17d28d32
Jaska <jlaska@gmail.com>
2017-08-19 19:20:57 -0700

    More delint

--- spherecluster/tests/test_common.py
+++ spherecluster/tests/test_common.py
@@ -1,6 +1,7 @@
 from sklearn.utils.estimator_checks import check_estimator
 from spherecluster import SphericalKMeans
 
+
 class TestCommon(object):
     def test_estimator_spherical_k_means(self):
         return check_estimator(SphericalKMeans)
