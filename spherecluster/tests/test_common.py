8a1cee9f45ca856664086000b3e5b47e3aa27237
Jaska <jlaska@gmail.com>
2018-09-08 21:13:03 -0700

    Bump version

--- spherecluster/tests/test_common.py
+++ spherecluster/tests/test_common.py
@@ -1,10 +1,7 @@
 from sklearn.utils.estimator_checks import check_estimator
-from spherecluster import SphericalKMeans, VonMisesFisherMixture
+from spherecluster import SphericalKMeans
 
 
 def test_estimator_spherical_k_means():
     return check_estimator(SphericalKMeans)
 
-
-# def test_estimator_von_mises_fisher():
-#    return check_estimator(VonMisesFisherMixture)
